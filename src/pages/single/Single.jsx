6798a5c59c8638e60c85eaf59796b2bff1a403d7
naseem umar masaid <naseem1masaid@gmail.com>
2024-06-28 19:57:22 +0700

    main

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -2,9 +2,13 @@ import "./single.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
-import List from "../../components/datatable/Datatable";
+import Datatable from "../../components/datatable/Datatable";
+import { useLocation } from "react-router-dom"
 
-const Single = () => {
+const Single = ({ columns }) => {
+  const location = useLocation();
+  const id = location.pathname.split('/')[2];
+
   return (
     <div className="single">
       <Sidebar />
@@ -16,15 +20,19 @@ const Single = () => {
             <h1 className="title">Information</h1>
             <div className="item">
               <img
-                src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTDS-NN32oWj6jIoj_Yo-XDbjXAgkh7fLKNsrdYhuxxQgdCkpxn"
                 alt=""
                 className="itemImg"
               />
               <div className="details">
-                <h1 className="itemTitle">Jane Doe</h1>
+                <h1 className="itemTitle">Cristiano Ronaldo</h1>
+                <div className="detailItem">
+                  <span className="itemKey">ID:</span>
+                  <span className="itemValue">{id}</span>
+                </div>
                 <div className="detailItem">
                   <span className="itemKey">Email:</span>
-                  <span className="itemValue">janedoe@gmail.com</span>
+                  <span className="itemValue">RonaldoGoat7@gmail.com</span>
                 </div>
                 <div className="detailItem">
                   <span className="itemKey">Phone:</span>
@@ -49,7 +57,7 @@ const Single = () => {
         </div>
         <div className="bottom">
           <h1 className="title">Last Transactions</h1>
-          <List />
+          <Datatable columns={columns} />
         </div>
       </div>
     </div>
