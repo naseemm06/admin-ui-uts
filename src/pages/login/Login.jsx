77808b434bcba3fcf586bbbd94af62196a282550
naseem umar masaid <naseem1masaid@gmail.com>
2024-07-20 14:42:21 +0700

    main

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,14 +3,18 @@ import { useContext, useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
 import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../../context/AuthContext"
+import { AuthContext } from "../../context/AuthContext";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import { DarkModeContext } from "../../context/darkModeContext";
 
 const Login = () => {
   const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const navigate = useNavigate()
+  const navitage = useNavigate()
+  const { darkMode, dispatch: darkModeDispatch } = useContext(DarkModeContext);
   const { dispatch } = useContext(AuthContext);
 
   const handleLogin = (e) => {
@@ -21,7 +25,7 @@ const Login = () => {
         // Signed in
         const user = userCredential.user;
         dispatch({ type: "LOGIN", payload: user });
-        navigate("/");
+        navitage("/")
       })
       .catch((error) => {
         setError(true);
@@ -43,16 +47,22 @@ const Login = () => {
           placeholder="Enter your password"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit" data-testid="submit" >Login</button>
+        <button type="submit" data-testid="submit">Login</button>
         {error && <span>Wrong email or password!</span>}
+        <div className="item">
+          {darkMode ? (
+            <LightModeOutlinedIcon className="icon"
+              onClick={() => darkModeDispatch({ type: "TOGGLE" })} />
+          ) : (
+            <DarkModeOutlinedIcon className="icon"
+              onClick={() => darkModeDispatch({ type: "TOGGLE" })} />
+          )}
+        </div>
       </form>
     </div>
-
   );
-  <div className="bottom">
-    <div className="colorOption" onClick={() => dispatch({ type: "LIGHT" })}></div>
-    <div className="colorOption" onClick={() => dispatch({ type: "DARK" })}></div>
-  </div>
+
+
 };
 
 export default Login;
